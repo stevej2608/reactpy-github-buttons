fd26eb25386fa298cb86ade80420cb7deca8d049
Steve Jones <jonesst2608@gmail.com>
2023-12-14 11:07:39 +0000

    Bump version 0.0.4

--- reactpy_github_buttons/__init__.py
+++ reactpy_github_buttons/__init__.py
@@ -1,5 +1,5 @@
 # the version is statically loaded by setup.py
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from .buttons import (
     FollowButton,StarButton,SponsorButton, WatchButton, ForkButton,
