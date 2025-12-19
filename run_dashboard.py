73d8682540f6e0e51d267e75fae8221bd516603a
Steve Jones <jonesst2608@gmail.com>
2025-12-19 11:39:09 +0000

    Removed unused code

--- run_dashboard.py
+++
@@ -1,7 +0,0 @@
-from examples.button_playground import AppMain
-from utils.fast_server import run
-
-# python run_dashboard.py
-
-if __name__ == "__main__":
-    run(AppMain, disable_server_logs=True)
