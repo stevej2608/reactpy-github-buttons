c8b31194e1ab066da84b2e3db6aed47fac6eddf4
Steve Jones <jonesst2608@gmail.com>
2026-01-06 14:48:12 +0000

    Upgraded to reactpy v2.0.0b7

--- src/reactpy_github_buttons/__init__.py
+++ src/reactpy_github_buttons/__init__.py
@@ -1,5 +1,5 @@
 # the version is statically loaded by Hatch from pyproject.toml
-__version__ = "0.0.19b4"
+__version__ = "0.0.20b7"
 
 from .button_wrapper import (
     FollowButton,StarButton,SponsorButton, WatchButton, ForkButton,
